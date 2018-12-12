1a238a830c4a8e1ac2369b49721a3d73935c5037
Mike Woudenberg <mwoudenberg@xebia.com>
2018-12-12 12:53:20 +0100

    Adds solution for day 12 part 1

---
+++ day-12/part1.js
@@ -0,0 +1,49 @@
+var readline = require('readline');
+
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false
+});
+
+
+let initialState;
+let rules = [];
+
+let stateRegex = /initial state: (.+)/
+let ruleRegex = /(.+) => (.)/
+rl.on('line', function (line) {
+  let state = stateRegex.exec(line);
+  if (state) {
+    initialState = state[1];
+    return;
+  }
+  let rule = ruleRegex.exec(line);
+  if (rule) {
+    rules.push({in: rule[1], out: rule[2]})
+    return;
+  }
+});
+
+function applyRules(line) {
+  let result = line.replace(/./g, '.');
+  rules.forEach(rule => {
+    let index = -1;
+    for (;;) {
+      index = line.indexOf(rule.in, index + 1);
+      if (index === -1) {
+        break;
+      }
+      result = result.substring(0, index + 2) + rule.out + result.substring(index + 3);
+    } 
+  })
+  return result;
+}
+
+rl.on('close', function() {
+  let result = initialState;
+  for (let i=0; i<20; i++) {
+    result = applyRules(`..${result}..`);
+  }
+  console.log([...result].reduce((memo, value, id) => memo + (value === '#' ? id - 40: 0) , 0));  
+})
